7b68e738001b0afcd8ad6448be95aa79055abb5b
nicolas PROCUREUR <nprocureur@gmail.com>
2017-10-09 11:44:46 +0200

    Extracts ReactiveMeasure plugin from Ekylibre + bundle as a npm module with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.coffee$/,
+        use: [ 'coffee-loader' ]
+      }
+    ]
+  },
+  entry: ['./src/reactive_measure.coffee', './src/geographic_util.coffee'],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'reactive_measure.js'
+  },
+  externals: {
+    'leaflet': 'L'
+  },
+  resolve: {
+    extensions: ['.coffee', '.js']
+  }
+}
