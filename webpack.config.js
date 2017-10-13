7382088b3b639c136f0cae8eb85d20094ab7dac9
nicolas PROCUREUR <nprocureur@gmail.com>
2017-10-13 09:32:24 +0200

    Extracts leaflet draw 0.3 patches

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
       }
     ]
   },
-  entry: ['./src/reactive_measure.coffee', './src/geographic_util.coffee'],
+  entry: ['./src/reactive_measure.coffee', './src/geographic_util.coffee', './src/leaflet.draw-0.3.patches.coffee'],
   output: {
     path: __dirname + '/dist',
     filename: 'reactive_measure.js'
